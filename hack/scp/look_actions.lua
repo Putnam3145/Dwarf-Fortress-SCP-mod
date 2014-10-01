e877bb42d5fb424e524f87c0c6ae1782418fd92e
Putnam3145 <bpwhite@cableone.net>
2014-09-30 23:19:29 -0700

    Updated a bunch of crap to work with 0.40.

--- hack/scp/look_actions.lua
+++ hack/scp/look_actions.lua
@@ -11,6 +11,9 @@ function run(looker,lookee)
 		looker.relations.old_year=df.global.cur_year+1
 		dfhack.run_script('scp/mark_for_death','-target',looker.id,'-type','023')
 	end
+	if df.creature_raw.find(lookee.race).creature_id=='SCP_173' then
+		dfhack.run_script('modtools/add-syndrome','-syndrome','scp 173 stopped','-resetPolicy','ResetDuration','-target',lookee.id,'-skipImmunities')
+	end
 end
 
 run(df.unit.find(args.looker),df.unit.find(args.lookee))
